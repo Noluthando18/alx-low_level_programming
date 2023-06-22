2f7e8caafcbb095869cc3a17e26f7aac0937aac3
Noluthando18 <Noluthandombatha50@gmail.com>
2023-06-21 17:54:18 -0700

    Task 1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+	int m;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	m =n % 10;
+	if (m > 5)
+		printf("Las"
+	return (0);
+}
